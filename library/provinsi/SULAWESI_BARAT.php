40be5c6b115e390908721dedbd6af704ba311292
Ogie Nurdiana <ogie.nurdiana447@gmail.com>
2024-09-13 11:15:17 +0700

    feat(Library): add SULAWESI_BARAT

--- library/provinsi/SULAWESI_BARAT.php
+++ library/provinsi/SULAWESI_BARAT.php
@@ -19,38 +19,48 @@ class SULAWESI_BARAT
 
         // Check if response is valid
         if ($response && !is_null($response) && $response !== false) {
-
+            $response = json_decode($response, true);
+            
+            if (!$response) {
+                return null;
+            }
+            
             // Map extracted data to response data
-            // $responseData = [
-            //     'MERK'          => $textParts[],
-            //     'MODEL'         => $textParts[],
-            //     'JENIS'         => $textParts[],
-            //     'TAHUN'         => (int) $textParts[],
-            //     'WARNA'         => $textParts[],
-            //     'CC'            => (int) $textParts[],
-            //     'BBM'           => $textParts[],
-            //     'PLAT'          => $textParts[],
-            //     'NO_POLISI'     => implode('', $requestData),
-            //     'NO_RANGKA'     => $textParts[],
-            //     'NO_MESIN'      => $textParts[],
-            //     'NAMA_PEMILIK'  => $textParts[],
-            //     'ALAMAT'        => $textParts[],
+            $responseData = [
+                'MERK'          => $response[0]['koding']['merek'],
+                'MODEL'         => $response[0]['koding']['model'],
+                'JENIS'         => $response[0]['jeniskb']['nama_jenis_kendaraan'],
+                'TAHUN'         => (int) $response[0]['th_buatan'],
+                'WARNA'         => $response[0]['warna_kb'],
+                'CC'            => (int) $response[0]['jumlah_cc'],
+                // 'BBM'           => $response[0][''],
+                // 'PLAT'          => $response[0][''],
+                'NO_POLISI'     => implode('', $data),
+                'NO_RANGKA'     => $response[0]['no_chasis'],
+                'NO_MESIN'      => $response[0]['no_mesin'],
+                'NAMA_PEMILIK'  => $response[0]['nm_pemilik'],
+                'ALAMAT'        => $response[0]['al_pemilik'],
 
-            //     'MILIK_KE'      => (int) $textParts[],
-            //     'WILAYAH'       => $textParts[],
-            //     'TGL_PAJAK'     => preg_replace('/(\d{2})-(\d{2})-(\d{4})/', '$3$2$1', $textParts[]),
-            //     'TGL_STNK'      => preg_replace('/(\d{2})-(\d{2})-(\d{4})/', '$3$2$1', $textParts[]),
-            //     'PKB_POKOK'     => (int) str_replace(['.', ','], '', $textParts[]),
-            //     'PKB_DENDA'     => (int) str_replace(['.', ','], '', $textParts[]),
-            //     'SWDKLLJ_POKOK' => (int) str_replace(['.', ','], '', $textParts[]),
-            //     'SWDKLLJ_DENDA' => (int) str_replace(['.', ','], '', $textParts[]),
-            //     'PNPB_STNK'     => (int) str_replace(['.', ','], '', $textParts[]),
-            //     'PNPB_DENDA'    => (int) str_replace(['.', ','], '', $textParts[]),
-            //     'TOTAL'         => (int) str_replace(['.', ','], '', $textParts[]),
-            // ];
+                'MILIK_KE'      => (int) $response[0]['kd_jen_mohon'],
+                'WILAYAH'       => $response[0]['kode_samsat'],
+                'TGL_PAJAK'     => preg_replace('/(\d{2})-(\d{2})-(\d{4})/', '$3$2$1', $response[0]['tg_akhir_pkb']),
+                'TGL_STNK'      => preg_replace('/(\d{2})-(\d{2})-(\d{4})/', '$3$2$1', $response[0]['tg_akhir_stnk']),
+                'PKB_POKOK'     => (int) str_replace(['.', ','], '', $response[0]['bea_pkb_pok']),
+                'PKB_DENDA'     => (int) str_replace(['.', ','], '', $response[0]['tunggakan']['den_pkb']),
+                'SWDKLLJ_POKOK' => (int) str_replace(['.', ','], '', $response[0]['bea_swdkllj_pok']),
+                'SWDKLLJ_DENDA' => (int) str_replace(['.', ','], '', $response[0]['tunggakan']['den_swd']),
+                // 'PNPB_STNK'     => (int) str_replace(['.', ','], '', $response[0]['']),
+                // 'PNPB_DENDA'    => (int) str_replace(['.', ','], '', $response[0]['']),
+                'TOTAL'         => (int) str_replace(['.', ','], '', (
+                    (int) str_replace(['.', ','], '', $response[0]['bea_pkb_pok']) + 
+                    (int) str_replace(['.', ','], '', $response[0]['tunggakan']['den_pkb']) + 
+                    (int) str_replace(['.', ','], '', $response[0]['bea_swdkllj_pok']) + 
+                    (int) str_replace(['.', ','], '', $response[0]['tunggakan']['den_swd'])
+                )),
+            ];
 
             // Validate and return response data
-            // return ResponseValidator::validate($responseData);
+            return ResponseValidator::validate($responseData);
         }
 
         return null;
